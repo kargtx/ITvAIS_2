baa63d9b331d9e98976cc414ee53e0003c1775f6
Karim Garifullin <kargtx@yandex.ru>
2024-04-06 01:15:08 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+        {
+            System.out.println("работаем ребятки");
+            System.out.println("разрабы Ильсур Данил, модератор Карим");
+        }
+}
