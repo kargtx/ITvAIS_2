272e2e6f1af82b3402cd84dffe0d209fe1db7e9c
Karim Garifullin <kargtx@yandex.ru>
2024-04-07 21:48:46 +0500

    поправка теста

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main
     public static void main(String[] args)
         {
         System.out.println("Hello world!");
-        }
         System.out.print("Введите число, первые 4 степени которого хотите узнать: ");
+        }
+
 }
