538ce2010692255869157a58ac1de4d6c542dcca
Gainanov Danil <GDIm4n@yandex.ru>
2024-04-12 20:26:31 +0500

    первый коммит, создание текстовых полей

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Font;
+
 public class Main
 {
-    public static void main(String[] args)
-        {
-        System.out.println("Hello world!");
-        System.out.println("Введите число, первые 4 степени которого хотите узнать: ");
-        System.out.println("здарова пацаны");
-        }
+    public static void main(String[] args) throws Exception
+    {
+        new EnterF();
+    }
+}
 
+
+//класс графики пользовательского окна и ввода/вывода данных
+class EnterF extends JFrame {
+    //создание переменных
+    int b = 1;
+    double x1 = -5;
+    double x2 = 5;
+
+    //создание текстовых полей
+    TextField Tx1;
+    TextField Tx2;
+    TextField Tf;
+    //создание окна
+    JFrame JF;
+    JPanel JP;
+    JButton bt, bt2;
+    JLabel answ;
+    Font font = new Font("default", Font.BOLD, 15);
+
+    EnterF() {
+    }
 }
